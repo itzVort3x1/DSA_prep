6ef6d2fde48d0022ca6cbbc5bb56cd7d20e75321
itzVort3x1 <saikaustubh10@gmail.com>
2021-11-06 14:02:22 +0530

    added comments

--- basics/basic-problems.py
+++ basics/basic-problems.py
@@ -1,12 +1,17 @@
+# ----------------------------------
 # Sum of N natural numbers
-# userInput = int(input("Please enter a number: "))
-# total = 0
-# for val in range(1, userInput + 1):
-#     total = total + val
+# ----------------------------------
+userInput = int(input("Please enter a number: "))
+total = 0
+for val in range(1, userInput + 1):
+    total = total + val
 
-# print("The sum of Natural Numbers from 1 to {0} = {1}".format(userInput, total))
+print("The sum of Natural Numbers from 1 to {0} = {1}".format(userInput, total))
 
+
+# ----------------------------------
 # Factorial of a number
+# ----------------------------------
 userInput = int(input("Please enter a number: "))
 def factorial(n):
     fact = 1
@@ -15,3 +20,4 @@ def factorial(n):
     return fact
 total = factorial(userInput)
 print("The factorial of {0} is {1}".format(userInput, total))
+
