fca77e0fd39f9dff9b6133db79882140292e625f
itzVort3x1 <saikaustubh10@gmail.com>
2021-11-06 13:48:49 +0530

    first commit

---
+++ basics/python-basics.py
@@ -0,0 +1,13 @@
+# lists
+list = [1,2,3,4,5,6,7]
+print(list)
+
+list1 = [1,2,3,4,5,6,7, 'hi', 'hello']
+print(list1)
+
+list1.append('new')
+print(list1)
+
+# dictonaries
+d1={'a':1, 'b':2, 'c':3}
+print(d1)
